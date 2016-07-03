ad24e1dd13f74789065e170a51d887f4937735e7
Peter Chudinov <peterchudinov@icloud.com>
2016-07-03 16:20:04 +0300

    rake populate

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,28 +13,6 @@ words_page = Nokogiri::HTML(open("http://languagedaily.com/learn-german/vocabula
 german_words = Array.new()
 english_words = Array.new()
 
-=begin
-german_words += words_page.css("tr[class=rowA]/td[class=bigLetter]/text()").to_a
-german_words += words_page.css("tr[class=rowB]/td[class=bigLetter]/text()").to_a
-#german_words.insert(35, words_page.css("tr[class=rowB]/td[class=bigLetter]/a/text()"))
-
-
-puts "german_words.count = #{german_words.count}"
-puts "german_words is #{german_words[1].class}"
-puts german_words.index('Sie')
-
-puts "======"
-
-english_words += words_page.css("tr[class=rowA]/td[3]/text()").to_a
-english_words += words_page.css("tr[class=rowB]/td[3]/text()").to_a
-
-puts "english_words.count = #{english_words.count}"
-
-keys = ["original_text", "translated_text"]
-h = Hash[keys.map {|x| [x, ""]}]
-puts h
-=end
-
 words_page.css("tr[class=rowA]/td[class=bigLetter]").each do |word|
   german_words.push(word.text)
 end
@@ -43,27 +21,13 @@ words_page.css("tr[class=rowB]/td[class=bigLetter]").each do |word|
   german_words.push(word.text)
 end
 
-#puts "german_words.count = #{german_words.count}"
-#puts "german_word is #{german_words[0].class}"
-
-#puts "======"
-
-words_page.css("tr[class=rowA]/td[3]").each do |translation|
-	english_words.push(translation.text)
+words_page.css("tr[class=rowA]/td[3]").each do |word|
+	english_words.push(word.text)
 end
 
-words_page.css("tr[class=rowB]/td[3]").each do |translation|
-	english_words.push(translation.text)
+words_page.css("tr[class=rowB]/td[3]").each do |word|
+	english_words.push(word.text)
 end
-
-#puts "english_words.count = #{english_words.count}"
-#puts "english_word is #{english_words[0].class}"
-
-#german_words.each do |w|
-  #puts "'#{german_words.index(w)}' => '#{w}' => '#{english_words[german_words.index(w)]}'"
-#end
-
-#puts "======"
 
 hashes = Array.new
 german_words.each do |w|
@@ -71,20 +35,9 @@ german_words.each do |w|
   h[:original_text] = w
   h[:translated_text] = english_words[german_words.index(w)]
   hashes.push(h)
-  #puts "'#{w}' got into hash"
-end
-
-#@hash = hashes[0]
-#puts @hash[:original_text]
-
-
-hashes.each do |h|
   @active_hash = h
-  puts "attempting to put '#{@active_hash[:original_text]}' into cards..."
   Card.create(
   	original_text: @active_hash[:original_text],
     translated_text: @active_hash[:translated_text]
     )
-  puts "'#{@active_hash[:original_text]}' put into Cards"
 end
-
--- lib/tasks/populate.rake
+++ lib/tasks/populate.rake
@@ -1,14 +1,68 @@
 require 'open-uri'
+hashes = Hash.new
 
-loop do |id|
-  words_page = Nokogiri::HTML(open("http://languagedaily.com/learn-german/vocabulary/common-german-words-{#{id}"))
+task :populate => [:environment] do
 
+  get_words(Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words")))
+  get_words(Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/most-common-german-words-2")))
+  loop_through_pages
+
+  puts "imported #{hashes.count} hashes (#{Card.count} successful/#{hashes.count} total)"
+
+end
+
+def loop_through_pages
+  id = 2
+
+  loop do
+    id += 1
+    begin
+      words_page = Nokogiri::HTML(open("http://languagedaily.com/learn-german/vocabulary/common-german-words-#{id}"))
+    rescue OpenURI::HTTPError => e
+      if e.message == 'no more pages to parse...'
+        break
+      end
+    end
+    get_words(words_page)
+
+  end
+end
+
+
+def get_words(page)
   german_words = Array.new()
   english_words = Array.new()
 
-  
+  page.css("tr[class=rowA]/td[class=bigLetter]").each do |word|
+    german_words.push(word.text)
+    english_words.push(page.css("tr[class=rowA]/td[3]")[german_words.index(word.text)].text)
+  end
 
-  rescue OpenURI::HTTPError => e
-    break if e.message == '404 Not Found'
+  page.css("tr[class=rowB]/td[class=bigLetter]").each do |word|
+    german_words.push(word.text)
+    english_words.push(page.css("tr[class=rowB]/td[3]")[german_words.index(word.text)].text)
+  end
+
+#, tr[class=rowB]/td[class=bigLetter]
+  #page.css("tr[class=rowA]/td[class=3], tr[class=rowB]/td[3]").each do |word|
+   # english_words.push(word.text)
+  #end
+
+  hashes = Array.new
+  german_words.each do |w|
+    h = Hash.new
+
+    h[:original_text] = w
+    h[:translated_text] = english_words[german_words.index(w)]
+    hashes.push(h)
+    @active_hash = h
+    puts h
+#===========================================
+    @card = Card.create(
+      original_text: @active_hash[:original_text],
+      translated_text: @active_hash[:translated_text]
+    )
+    puts @card
+#===========================================
   end
 end
