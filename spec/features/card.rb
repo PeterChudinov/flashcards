60b60701ad067d2aad0e56cb1b00265ce47e9a22
Peter Chudinov <peterchudinov@icloud.com>
2016-07-25 18:25:33 +0300

    fix specs, fix home/index

--- spec/features/card.rb
+++ spec/features/card.rb
@@ -2,7 +2,7 @@ require 'capybara/rspec'
 
 Capybara.describe 'card trainer', :type => :feature do
 
-  before :each do
+  before :example do
     @card = FactoryGirl.create(:card)
     @card.set_test_review_date!
   end
@@ -10,7 +10,6 @@ Capybara.describe 'card trainer', :type => :feature do
   it 'checks if the user can do the card quiz correctly' do
     visit root_path
     fill_in 'response', :with => 'привет'
-    save_and_open_page
     click_button 'Проверить'
     expect(page).to have_content 'Верно!'
   end
@@ -18,7 +17,6 @@ Capybara.describe 'card trainer', :type => :feature do
   it 'checks if the user can do the card quiz incorrectly' do
     visit root_path
     fill_in 'response', :with => 'не знаю'
-    save_and_open_page
     click_button 'Проверить'
     expect(page).to have_content 'Неверно!'
   end
