7cbe107ad722e0e5132c8e10fc3f8e9419c86425
Peter Chudinov <peterchudinov@icloud.com>
2016-08-11 16:33:56 +0300

    add support for eng/rus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :require_signin
+  before_action :set_locale
 
   private
   
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
 
   def user
     @user ||= current_user
+  end
+ 
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,11 @@
 class Card < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, styles: {medium: "300x300#", thumb: "20x20#"}
+  has_attached_file :image, styles: { medium: "300x300#", thumb: "20x20#" }
   validates_attachment :image,
-    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+    content_type: {
+      content_type: ["image/jpeg", "image/gif", "image/png"]
+    }
 
   before_validation(on: :create) do
     self.review_date = 3.days.from_now.end_of_day
