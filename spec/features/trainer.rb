7978faf4b71346f2f85d7e21e0431bd21da4eeed
Peter Chudinov <peterchudinov@icloud.com>
2016-09-22 11:17:03 -0700

    trainer spec upd

--- spec/features/trainer.rb
+++ spec/features/trainer.rb
@@ -35,11 +35,10 @@ Capybara.describe 'card trainer feature spec', type: :feature do
     fill_in 'response', with: 'привет'
     click_button 'Check'
 
-    Timecop.travel(1.day.from_now.end_of_day) do
-      visit root_path
-      fill_in 'response', with: 'привет'
-      click_button 'Check'
-      expect(page).to have_content "No more cards for now, wait until tomorrow"
-    end
+    Timecop.travel(1.day.from_now.end_of_day)
+    visit root_path
+    fill_in 'response', with: 'привет'
+    click_button 'Check'
+    expect(page).to have_content "No more cards for now, wait until tomorrow"
   end
 end
